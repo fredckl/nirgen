15ddf20685bb2931d9dc75cf11273760610140b3
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-07-29 10:37:31 +0200

    ++

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fredckl/react-memo-form.git master:gh-pages
+git push -f git@github.com:fredckl/nirgen.git master:gh-pages
 
 cd -
